c876039f8ae72bc055a481196c4bace15732bd17
philosopher <200029096@must.edu.eg>
2026-08-10 17:33:46 +0300

    Adding Product Entity

--- src/EShop.Console/Entities/Category.cs
+++ src/EShop.Console/Entities/Category.cs
@@ -8,4 +8,5 @@ public class Category
     public string Name { get; set; }
     public Guid? ParentCategoryId { get; set; }
     public List<Category> SubCategories { get; set; }
+    public List<Product> Products { get; set; }
 }
