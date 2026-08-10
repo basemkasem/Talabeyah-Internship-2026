321afc3f434c7aec6416475e007c564c8328cece
philosopher <200029096@must.edu.eg>
2026-08-10 17:26:20 +0300

    Adding Category Entity

---
+++ src/EShop.Console/Entities/Category.cs
@@ -0,0 +1,11 @@
+using System.Runtime.CompilerServices;
+
+namespace EShop.Console.Entities;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Guid? ParentCategoryId { get; set; }
+    public List<Category> SubCategories { get; set; }
+}
