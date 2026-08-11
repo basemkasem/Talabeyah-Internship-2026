004aaea5f0a50619e9de0c72cef5e59a33a25c7a
philosopher <200029096@must.edu.eg>
2026-08-11 07:29:41 +0300

    Updating Category Entity

--- src/EShop.Console/Entities/Category.cs
+++ src/EShop.Console/Entities/Category.cs
@@ -1,12 +1,33 @@
-using System.Runtime.CompilerServices;
-
 namespace EShop.Console.Entities;
 
 public class Category
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public Guid? ParentCategoryId { get; set; }
-    public List<Category> SubCategories { get; set; }
-    public List<Product> Products { get; set; }
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public Guid? ParentCategoryId { get; private set; }
+    public List<Category> SubCategories { get; private set; } = new();
+    public List<Product> Products { get; private set; } = new();
+
+    public Category(Guid id, string name, Guid? parentCategoryId)
+    {
+        if (id == Guid.Empty)
+            throw new ArgumentException("Id cannot be empty");
+
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Name cannot be null or whitespace.");
+
+        Id = id;
+        Name = name;
+        ParentCategoryId = parentCategoryId;
+    }
+
+    public void AddSubCategory(Category subCategory)
+    {
+        SubCategories.Add(subCategory);
+    }
+
+    public void AddProduct(Product product)
+    {
+        Products.Add(product);
+    }
 }
