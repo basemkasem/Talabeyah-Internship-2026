4a23414462f1a22a8b4181fb576110d8cb6ea7dc
basem <basem.kasem16@gmail.com>
2026-08-19 14:41:09 +0300

    add auth service to make a post request and return a new token

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,11 +7,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     importProvidersFrom(NgbModule),
+    provideHttpClient()
   ],
 };
