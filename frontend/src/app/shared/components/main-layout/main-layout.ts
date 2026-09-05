bb4f1fef0848a54f41610d690bc18d3ea9b657e2
basem <basem.kasem16@gmail.com>
2026-09-05 15:37:23 +0300

    move navbar to a separate component and integrate into main layout

--- frontend/src/app/shared/components/main-layout/main-layout.ts
+++ frontend/src/app/shared/components/main-layout/main-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
